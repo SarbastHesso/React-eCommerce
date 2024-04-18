414d5dbe272b1b6e9a04022a6af8b775597f41d4
Sarbast Hesso <sarbast.hesso@gmail.com>
2024-04-18 12:39:50 +0200

    update createStore

--- src/store/index.js
+++ src/store/index.js
@@ -1,28 +1,27 @@
-import {createStore, compose, applyMiddleware} from 'redux';
-import thunk from 'redux-thunk';
-import rootReducer from './reducers/rootReducer';
+import { createStore, applyMiddleware, compose } from "redux";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers/rootReducer";
 
 const initialState = {
-    userLogInReducer: {
-      userInfo: localStorage.getItem('userInfo')
-        ? JSON.parse(localStorage.getItem('userInfo'))
-        : null,
-    },
-    cartReducer: {
-      cartProducts: localStorage.getItem('cartProducts')
-        ? JSON.parse(localStorage.getItem('cartProducts'))
-        : [],
-      shipping: localStorage.getItem('shipping')
-        ? JSON.parse(localStorage.getItem('shipping'))
-        : [],
-    },
-  };
+  userLogInReducer: {
+    userInfo: localStorage.getItem("userInfo")
+      ? JSON.parse(localStorage.getItem("userInfo"))
+      : null,
+  },
+  cartReducer: {
+    cartProducts: localStorage.getItem("cartProducts")
+      ? JSON.parse(localStorage.getItem("cartProducts"))
+      : [],
+    shipping: localStorage.getItem("shipping")
+      ? JSON.parse(localStorage.getItem("shipping"))
+      : [],
+  },
+};
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const store = createStore(
-    rootReducer,
-    initialState,
-    compose(
-        applyMiddleware(thunk),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    )
+  rootReducer,
+  initialState,
+  composeEnhancers(applyMiddleware(thunk))
 );
